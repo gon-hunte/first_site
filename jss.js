b5bf908a79aa0d1da90095e6a2f4dcd3f9e1bd9f
ayoub <ayo-oub1996@gmail.com>
2024-02-24 13:30:26 +0100

    add picture

---
+++ jss.js
@@ -0,0 +1,34 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", this.window.scrollY > 0);
+})
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navmenu.classList.toggle('open')
+}
+
+
+
+
+
+
+
+
+
+let product = document.querySelector('.row');
+product.onclick = ()=>{
+    
+        product.classList.toggle('cl')
+
+}
+
+
+
+
+
+
